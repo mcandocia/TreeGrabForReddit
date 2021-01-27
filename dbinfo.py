85ddbaa7751c993fcb2cdecb3bfe5b879838e3a5
Max Candocia <mscandocia@gmail.com>
2021-01-26 23:56:15 -0600

    fix

--- dbinfo.py
+++ dbinfo.py
@@ -1,7 +1,7 @@
-database = 'DATABASE NAME'
-tablespace = 'TABLESPACE NAME'#try pg_default or pg_global if you haven't configured a different one
-username='USERNAME'
-password='PASSWORD'
+database = ''
+tablespace = ''
+username=''
+password=''
 host='localhost'
 port=5432
 dbtype='postgres'
