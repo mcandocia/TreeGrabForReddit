4d957c5c38cf219ef1186b3fdf48be6d144f305a
Max Candocia <mscandocia@gmail.com>
2021-01-26 23:55:38 -0600

    fix

--- scripts/build_profile.py
+++ scripts/build_profile.py
@@ -10,6 +10,12 @@ import argparse
 import os
 import psycopg2
 import db
+from color_printing import *
+
+try:
+    import regex as re
+except:
+    import re
 
 def get_options():
     parser = argparse.ArgumentParser(
@@ -30,6 +36,18 @@ def get_options():
     parser.add_argument(
         '--schema-2',
         help='The second schema to reference (later)'
+    )
+
+    parser.add_argument(
+        '--create-table',
+        action='store_true',
+        help='Creates a table instead of a materialized view'
+    )
+
+    parser.add_argument(
+        '--insert-into',
+        action='store_true',
+        help='Inserts non-existing entries into table instead of updating whole table'
     )
 
 
@@ -96,6 +114,8 @@ def get_options():
         default=20,
     )
 
+    parser.add_argument('--calculate-account-age', action='store_true', help='store account age as variable')
+
     parser.add_argument(
         '--drop-view',
         action='store_true',
@@ -104,7 +124,7 @@ def get_options():
 
     parser.add_argument(
         '--export',
-        help='File to export data to',
+        help='File to export data to. Not yet implemented.',
         required=False,
     )
 
@@ -113,11 +133,27 @@ def get_options():
         help='Do not create any views',
         action='store_true'
     )
+    '''
+    parser.add_argument(
+        '--record-most-recent-activity',
+        help='Record timestamps of most recent comment and most recent submission '
+        '(store NULL if unavailable, will probably use absurdly low TS value in later processing',
+        action='store_true'
+    )
+    '''
 
 
     args = parser.parse_args()
 
+            
+
     options = vars(args)
+    if options['insert_into']:
+        try:
+            assert options['create_table']
+        except AssertionError:
+            raise AssertionError('Must have "--create-table" if "--insert-into" is specified')
+    
 
     return options
                         
@@ -131,18 +167,22 @@ def main(options):
 
     if options['drop_view']:
         drop_query = drop_template.format(**options)
-        print(drop_query)
-        print('EXECUTING!')
+        if options['create_table']:
+            drop_query = re.sub('MATERIALIZED VIEW', 'TABLE', drop_query)
+        dimprint(drop_query)
+        bprint('EXECUTING!')
         cur.execute(drop_query)
 
 
 
     if options['gildings_1']:
         gilding_injection_part = gilding_injection
+        gilding_injection_user_part = gilding_injection_user
         
         subreddit_comment_part = ' \n'.join([
             subreddit_comment_rank_template_gilded.format(
-                n=n
+                n=n,
+                user_filter=''
             )
             for n in range(1, options['n_top_subreddits_for_comments']+1)
 
@@ -150,7 +190,8 @@ def main(options):
 
         subreddit_thread_part = ' \n'.join([
             subreddit_thread_rank_template_gilded.format(
-                n=n
+                n=n,
+                user_filter=''
             )
             for n in range(1, options['n_top_subreddits_for_threads']+1)
 
@@ -158,16 +199,20 @@ def main(options):
 
         domain_thread_part = ' \n'.join([
             domain_rank_template_gilded.format(
-                n=n
+                n=n,
+                user_filter=''
             )
             for n in range(1, options['n_top_domains_for_threads']+1)
 
         ])
     else:
         gilding_injection_part = ' '
+        gilding_injection_user_part = ' '
+        
         subreddit_comment_part = ' \n'.join([
             subreddit_comment_rank_template.format(
-                n=n
+                n=n,
+                user_filter=''
             )
             for n in range(1, options['n_top_subreddits_for_comments']+1)
 
@@ -175,7 +220,8 @@ def main(options):
 
         subreddit_thread_part = ' \n'.join([
             subreddit_thread_rank_template.format(
-                n=n
+                n=n,
+                user_filter=''
             )
             for n in range(1, options['n_top_subreddits_for_threads']+1)
 
@@ -183,23 +229,35 @@ def main(options):
 
         domain_thread_part = ' \n'.join([
             domain_rank_template.format(
-                n=n
+                n=n,
+                user_filter=''
             )
             for n in range(1, options['n_top_domains_for_threads']+1)
 
         ])
 
+    cte_part = cte_template.format(gilding_injection=gilding_injection_part, user_filter='', **options)
 
-
-
-    cte_part = cte_template.format(gilding_injection=gilding_injection_part, **options)
+    
+    if options['create_table']:
+        cte_part = re.sub('MATERIALIZED VIEW', 'TABLE', cte_part)
 
     if not options['gildings_2']:
         base_template_ = re.sub('(submissions|comments)_(silver|platinum),',' ', base_template)
     else:
         base_template_ = base_template
+
+    if options.get('calculate_account_age'):
+        account_age_part = ACCOUNT_AGE_PART
+    else:
+        account_age_part = ' '
         
-    base_part = base_template_.format(**options)
+    base_part = base_template_.format(
+        user_filter = '',
+        schema_1_gildings=gilding_injection_user_part,
+        account_age_part=account_age_part,
+        **options
+    )
 
     query = '\n'.join(
         [
@@ -212,14 +270,127 @@ def main(options):
     )
 
     if not options['skip_creation']:
-        print(query)
-        print("EXECUTING!")
+        dimprint(query)
+        bprint("EXECUTING!")
         cur.execute(query)
 
     cur.commit()
 
+    ## INSERT INTO
+    # mostly a copy/paste from above
+    # note that it uses blind insert, which means that it should only interact with tables created with
+    # this exact script
+    
+    if options['insert_into']:
+        user_filter = 'AND username NOT IN (SELECT username FROM {name})'.format(**options)
+        if options['gildings_1']:
+            gilding_injection_part = gilding_injection
+            gilding_injection_user_part = gilding_injection_user
+
+            subreddit_comment_part = ' \n'.join([
+                subreddit_comment_rank_template_gilded.format(
+                    n=n,
+                    user_filter=user_filter,
+                )
+                for n in range(1, options['n_top_subreddits_for_comments']+1)
+
+            ])
+
+            subreddit_thread_part = ' \n'.join([
+                subreddit_thread_rank_template_gilded.format(
+                    n=n,
+                    user_filter=user_filter,
+                )
+                for n in range(1, options['n_top_subreddits_for_threads']+1)
+
+            ])
+
+            domain_thread_part = ' \n'.join([
+                domain_rank_template_gilded.format(
+                    n=n,
+                    user_filter=user_filter,
+                )
+                for n in range(1, options['n_top_domains_for_threads']+1)
+
+            ])
+        else:
+            gilding_injection_part = ' '
+            gilding_injection_user_part = ' '
+            
+            subreddit_comment_part = ' \n'.join([
+                subreddit_comment_rank_template.format(
+                    n=n,
+                    user_filter=user_filter,
+                )
+                for n in range(1, options['n_top_subreddits_for_comments']+1)
+
+            ])
+
+            subreddit_thread_part = ' \n'.join([
+                subreddit_thread_rank_template.format(
+                    n=n,
+                    user_filter=user_filter,
+                )
+                for n in range(1, options['n_top_subreddits_for_threads']+1)
+
+            ])
+
+            domain_thread_part = ' \n'.join([
+                domain_rank_template.format(
+                    n=n,
+                    user_filter=user_filter,
+                )
+                for n in range(1, options['n_top_domains_for_threads']+1)
+
+            ])
+
+
+        cte_part = cte_template.format(
+            gilding_injection=gilding_injection_part, user_filter=user_filter, **options
+        )
+        
+        cte_part = re.sub('MATERIALIZED VIEW', 'TABLE', cte_part)
+        # change from CREATE to INSERT INTO
+        cte_part = re.sub('CREATE TABLE (.+?) AS', r'INSERT INTO \1 ', cte_part)
+        cte_part = re.sub('IF NOT EXISTS',' ', cte_part)
+
+        if not options['gildings_2']:
+            base_template_ = re.sub('(submissions|comments)_(silver|platinum),',' ', base_template)
+        else:
+            base_template_ = base_template
+
+        base_part = base_template_.format(
+            user_filter = '',
+            schema_1_gildings=gilding_injection_user_part,
+            account_age_part = account_age_part,
+            **options
+        )
+
+        query = '\n'.join(
+            [
+                cte_part,
+                base_part,
+                subreddit_comment_part,
+                subreddit_thread_part,
+                domain_thread_part
+            ] + [
+                'WHERE username NOT IN (SELECT username FROM {name})'.format(
+                    name=options['name']
+                )
+            ]
+        )
+
+        dimprint(query)
+        
+        cur.execute(query)
+        
+        cur.commit()
+        bprint('INSERTED!')
+
     if options['export']:
         pass
+
+    gprint("Done!")
 
 
 
@@ -228,8 +399,8 @@ def main(options):
 
 # CTEs and definition
 
+'''
 cte_alt_template = "CREATE MATERIALIZED VIEW IF NOT EXISTS {name} AS "
-
 cte_alt_definitions_templates = [
     """
 CREATE TEMPORARY TABLE user_subreddit_comment_stats AS ( SELECT * FROM (
@@ -269,6 +440,7 @@ SELECT *, row_number() OVER ( PARTITION BY username ORDER BY domain_count DESC) 
 )
 """
 ]
+'''
 
 cte_template = """
 CREATE MATERIALIZED VIEW IF NOT EXISTS {name} AS 
@@ -282,7 +454,7 @@ user_subreddit_comment_stats AS ( SELECT * FROM (
   FROM {schema_1}.comments 
   GROUP BY subreddit, author_name
   ORDER BY comment_count DESC) i1 ) ii1
-    WHERE subreddit_rank <= {n_top_subreddits_for_comments}
+    WHERE subreddit_rank <= {n_top_subreddits_for_comments} {user_filter}
 ),
 user_subreddit_thread_stats AS ( SELECT * FROM (
   SELECT *, row_number() OVER ( PARTITION BY username ORDER BY thread_count DESC) AS subreddit_rank FROM 
@@ -293,7 +465,7 @@ user_subreddit_thread_stats AS ( SELECT * FROM (
   FROM {schema_1}.threads 
   GROUP BY subreddit, author_name
   ORDER BY thread_count DESC) i2 ) ii2
-    WHERE subreddit_rank <= {n_top_subreddits_for_threads}
+    WHERE subreddit_rank <= {n_top_subreddits_for_threads} {user_filter}
 ),
 user_domain_stats AS ( SELECT * FROM (
 SELECT *, row_number() OVER ( PARTITION BY username ORDER BY domain_count DESC) AS domain_rank FROM 
@@ -304,7 +476,7 @@ SELECT *, row_number() OVER ( PARTITION BY username ORDER BY domain_count DESC) 
   FROM {schema_1}.threads
   GROUP BY domain, author_name
   ORDER BY domain_count DESC) i3 ) ii3
-  WHERE domain_rank <= {n_top_domains_for_threads}
+  WHERE domain_rank <= {n_top_domains_for_threads} {user_filter}
 )
 
 
@@ -313,12 +485,15 @@ SELECT *, row_number() OVER ( PARTITION BY username ORDER BY domain_count DESC) 
 
 
 gilding_injection = """, sum(silver) AS total_silver, sum(platinum) AS total_platinum"""
+gilding_injection_user = """ submissions_silver AS historic_submissions_silver, submissions_gold AS historic_submissions_gold, submissions_platinum AS historic_submissions_platinum, comments_silver AS historic_comments_silver, comments_gold AS historic_comments_gold, comments_platinum AS historic_comments_platinum, """
 
+
+# added id IS NOT NULL condition
 
 base_template = """
 
 SELECT *, CASE WHEN shadowban_flag = 1 or suspended_flag = 1 THEN 1 ELSE 0 END AS bad_flag 
-FROM (SELECT username, comment_karma AS historic_comment_karma, post_karma AS historic_post_karma, is_mod AS historic_is_mod, account_created, is_gold AS historic_gold, timestamp AS historic_timestamps, shadowbanned_by_date AS historic_shadowbanned_by_date, suspended_by_date AS historic_suspended_by_date FROM {schema_1}.user)  historic_user_data
+FROM (SELECT username, comment_karma AS historic_comment_karma, post_karma AS historic_post_karma, is_mod AS historic_is_mod, account_created, is_gold AS historic_gold, {schema_1_gildings} timestamp AS historic_timestamps, shadowbanned_by_date AS historic_shadowbanned_by_date, suspended_by_date AS historic_suspended_by_date {account_age_part}  FROM {schema_1}.users WHERE id IS NOT NULL)  historic_user_data
 
 LEFT JOIN (
     SELECT username, shadowbanned_by_date, suspended_by_date, CASE WHEN shadowbanned_by_date IS NOT NULL THEN 1 ELSE 0 END AS shadowban_flag, CASE WHEN suspended_by_date  IS NOT NULL THEN 1 ELSE 0 END AS suspended_flag, submissions_silver, submissions_gold, submissions_platinum, comments_silver, comments_gold, comments_platinum, timestamp AS followup_timestamp, is_mod, comment_karma, post_karma
@@ -328,11 +503,11 @@ USING(username)
 
 -- these two are just to see if user hasn't posted in a while
 LEFT JOIN (
-    SELECT author_name AS username, max(created) AS last_comment_timestamp FROM {schema_2}.comments GROUP BY author_name
+    SELECT author_name AS username, max(created) AS last_comment_timestamp FROM {schema_1}.comments GROUP BY author_name
 ) last_comment
 USING(username)
 LEFT JOIN (
-    SELECT author_name AS username, max(created) AS last_thread_timestamp FROM {schema_2}.threads GROUP BY author_name
+    SELECT author_name AS username, max(created) AS last_thread_timestamp FROM {schema_1}.threads GROUP BY author_name
 ) last_thread
 USING(username)
 
@@ -355,6 +530,7 @@ sum(CASE WHEN score <= {strong_negative_thread_score_maximum} THEN 1 ELSE 0 END)
   GROUP BY author_name
 ) user_thread_stats 
 USING(username)
+{user_filter}
 
 """
 
@@ -364,7 +540,7 @@ LEFT JOIN (
     SELECT domain AS domain_{n}, username, total_score AS domain_{n}_total_thread_score, domain_count AS domain_{n}_count, total_negative_threads AS domain_{n}_total_negative_threads,
     total_positive_threads AS domain_{n}_total_positive_threads, total_gold AS domain_{n}_thread_gold
     FROM user_domain_stats 
-    WHERE domain_rank = {n}
+    WHERE domain_rank = {n} {user_filter}
 ) domains_{n}
 USING(username)
 
@@ -378,7 +554,7 @@ LEFT JOIN (
     SELECT domain AS domain_{n}, username, total_score AS domain_{n}_total_thread_score, domain_count AS domain_{n}_count, total_negative_threads AS domain_{n}_total_negative_threads,
     total_positive_threads AS domain_{n}_total_positive_threads, total_gold AS domain_{n}_thread_gold, total_silver AS domain_{n}_thread_silver, total_platinum AS domain_{n}_thread_platinum
     FROM user_domain_stats 
-    WHERE domain_rank = {n}
+    WHERE domain_rank = {n} {user_filter}
 ) domains_{n}
 USING(username)
 
@@ -391,7 +567,7 @@ LEFT JOIN (
     SELECT subreddit AS subreddit_{n}_threads, username, total_score AS subreddit_{n}_total_thread_score, thread_count AS subreddit_{n}_thread_count, total_negative_threads AS subreddit_{n}_total_negative_threads,
     total_positive_threads AS subreddit_{n}_total_positive_threads, total_gold AS subreddit_{n}_thread_gold
     FROM user_subreddit_thread_stats 
-    WHERE subreddit_rank = {n}
+    WHERE subreddit_rank = {n} {user_filter}
 ) sub_threads_{n}
 USING(username)
 
@@ -402,7 +578,7 @@ LEFT JOIN (
     SELECT subreddit AS subreddit_{n}_threads, username, total_score AS subreddit_{n}_total_thread_score, thread_count AS subreddit_{n}_thread_count, total_negative_threads AS subreddit_{n}_total_negative_threads,
     total_positive_threads AS subreddit_{n}_total_positive_threads, total_gold AS subreddit_{n}_thread_gold, total_silver AS subreddit_{n}_thread_silver, total_platinum AS subreddit_{n}_thread_platinum
     FROM user_subreddit_thread_stats 
-    WHERE subreddit_rank = {n}
+    WHERE subreddit_rank = {n} {user_filter}
 ) sub_threads_{n}
 USING(username)
 
@@ -414,7 +590,7 @@ LEFT JOIN (
     SELECT subreddit AS subreddit_{n}_comments, username, total_score AS subreddit_{n}_total_comment_score, comment_count AS subreddit_{n}_comment_count, total_negative_comments AS subreddit_{n}_total_negative_comments,
     total_positive_comments AS subreddit_{n}_total_positive_comments, total_gold AS subreddit_{n}_comment_gold, average_comment_length AS subreddit_{n}_average_comment_length
     FROM user_subreddit_comment_stats 
-    WHERE subreddit_rank = {n}
+    WHERE subreddit_rank = {n} {user_filter}
 ) sub_comments_{n}
 USING(username)
 
@@ -426,13 +602,15 @@ LEFT JOIN (
     SELECT subreddit AS subreddit_{n}_comments, username, total_score AS subreddit_{n}_total_comment_score, comment_count AS subreddit_{n}_comment_count, total_negative_comments AS subreddit_{n}_total_negative_comments,
     total_positive_comments AS subreddit_{n}_total_positive_comments, total_gold AS subreddit_{n}_comment_gold, average_comment_length AS subreddit_{n}_average_comment_length, total_silver AS subreddit_{n}_comment_silver, total_platinum AS subreddit_{n}_comment_platinum
     FROM user_subreddit_comment_stats 
-    WHERE subreddit_rank = {n}
+    WHERE subreddit_rank = {n} {user_filter} 
 ) sub_comments_{n}
 USING(username)
 
 """
 
 drop_template = "DROP MATERIALIZED VIEW IF EXISTS {name}"
+
+ACCOUNT_AGE_PART = ', EXTRACT(days FROM timestamp-account_created) AS account_age '
 
 
 ## execution
